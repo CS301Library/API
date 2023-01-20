91718c1d17c82e8fd469bb4c8a01309ed54ccd2a
Rizzzi Git <rizzzigit@gmail.com>
2023-01-20 19:53:17 +0800

    add top borrowed books property in greeting

--- src/core/handler/admin.ts
+++ src/core/handler/admin.ts
@@ -1,11 +1,11 @@
 import Express from 'express'
 
 import { Handler, HandlerReturn } from '../handler'
-import { Account, AccountRole, BorrowStatus } from '../resource'
+import { Account, AccountRole, Book, BorrowStatus } from '../resource'
 
 export const handle = async (main: Handler, request: Express.Request, response: Express.Response): Promise<HandlerReturn> => {
   const { auth, method, pathArray } = request
-  const { resources: { Account, Borrow } } = main
+  const { resources: { Account, Borrow, Book } } = main
 
   if (auth == null) {
     return main.errorStatus(401, 'AuthRequired')
@@ -16,9 +16,27 @@ export const handle = async (main: Handler, request: Express.Request, response: 
       switch (pathArray[1] ?? '') {
         case 'greet': {
           const borrowedBooksToday = await Borrow.count({ status: BorrowStatus.Borrowed, createTime: { $gt: (new Date(new Date().toISOString().slice(0, 10)).getTime()) } })
+          const topBorrowedBooksMap: Map<string, number> = new Map()
+
+          for await (const borrow of Borrow.find({ createTime: { $gt: Date.now() - (1000 * 60 * 60 * 24 * 30) } })) {
+            topBorrowedBooksMap.set(borrow.bookId, (topBorrowedBooksMap.get(borrow.bookId) ?? 0) + 1)
+          }
+
+          const topBorrowedBooks: Array<[Book, number]> = []
+          for (const [id, count] of Array.from(topBorrowedBooksMap.entries()).sort((a, b) => b[1] - a[1])) {
+            const book = await Book.findOne({ id })
+            if (book == null) {
+              continue
+            }
+            topBorrowedBooks.push([main.leanObject(book), count])
+            if (topBorrowedBooks.length >= 10) {
+              break
+            }
+          }
 
           return main.okStatus(200, {
-            borrowedBooksToday
+            borrowedBooksToday,
+            topBorrowedBooks
           })
         }
 
